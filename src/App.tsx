255c2f3492098ae9f6ddce97bde150f248c59cfc
Zeha Irawan <zehairawan@gmail.com>
2022-04-04 12:25:03 +0800

    use redux selector instead of useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React,{ useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BaseButton from './components/BaseButton';
+import { RootState } from './Redux/store';
+import {decrement, increment} from './Redux/counter'
 
 function App() {
 
- const [count, setCount] = useState(0);
+//  const [count, setCount] = useState(0);
+const { count } = useSelector((state: RootState) => state.counter);
+
+const dispatch = useDispatch();
 
   return (
     <div>
-        <BaseButton/>
-        <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
+      <BaseButton />
+      <p>You clicked {count} times</p>
+      {/* <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button> */}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
