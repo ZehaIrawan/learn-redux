5d0fc3dfd3dd6712a52f104cfde551709d395010
Zeha Irawan <zehairawan@gmail.com>
2022-04-04 13:51:24 +0800

    add incrementByAmount dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import BaseButton from './components/BaseButton';
 import { RootState } from './Redux/store';
-import {decrement, increment} from './Redux/counter'
+import { decrement, increment, incrementByAmount } from './Redux/counter';
 
 function App() {
+  //  const [count, setCount] = useState(0);
+  const { count } = useSelector((state: RootState) => state.counter);
 
-//  const [count, setCount] = useState(0);
-const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+  const [amount, setAmount] = useState<number>(0);
 
   return (
     <div>
@@ -19,6 +20,14 @@ const dispatch = useDispatch();
       <button onClick={() => setCount(count - 1)}>-</button> */}
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setAmount(parseInt(e.target.value))
+        }
+        />
+        <button onClick={() => dispatch(incrementByAmount(amount))}>Increase by {amount}</button>
     </div>
   );
 }
