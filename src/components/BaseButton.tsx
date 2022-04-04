112e462183a7b9d3654abf1c4f3115edbbb5f32b
Zeha Irawan <zehairawan@gmail.com>
2022-04-04 10:09:44 +0800

    :art:  setup material ui

---
+++ src/components/BaseButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const BaseButton = () => {
+  return (<Button variant="contained">Text</Button>);
+}
+
+export default BaseButton
